6c4e77883e31de78c115d7178c16c37628d6ed2c
vivek22117 <vivekmishra22117@gmail.com>
2021-06-12 20:17:06 +0530

    adding constant for Application UIRs

--- auth-api/src/main/java/com/dd/auth/api/util/AppUtility.java
+++ auth-api/src/main/java/com/dd/auth/api/util/AppUtility.java
@@ -22,4 +22,10 @@ public class AppUtility {
     public static final String WEATHER_DELETE_ALL_API = "/deleteAll/{username}";
 
 
+    public static final String API_AUTH_ROOT_URI = "/api/auth";
+    public static final String AUTH_SIGNUP_URI = "/signup";
+    public static final String ACCOUNT_VERIFICATION_TOKEN_URI = "/accountVerification/{token}";
+    public static final String AUTH_LOGIN_URI = "/login";
+    public static final String AUTH_REFRESH_TOKEN_URI = "/refresh/token";
+    public static final String AUTH_LOGOUT_URI = "/logout";
 }
