0df4330e91874beae76f2aa9a73a5dd1e848894d
vivek22117 <vivekmishra22117@gmail.com>
2021-06-14 20:44:55 +0530

    adding swagger configuration

--- auth-api/src/main/java/com/dd/auth/api/AuthServiceApplication.java
+++ auth-api/src/main/java/com/dd/auth/api/AuthServiceApplication.java
@@ -1,12 +1,18 @@
 package com.dd.auth.api;
 
+import com.dd.auth.api.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "local");
+
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 
