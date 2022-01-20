9768006e0b5687fa2523f03b3444ac6518e5437c
vivek22117 <vivekmishra22117@gmail.com>
2022-01-20 12:47:28 +0530

    formatting module impl script

--- deployment/ecr-resoruces/main.tf
+++ deployment/ecr-resoruces/main.tf
@@ -4,12 +4,14 @@
 module "vpc-es-cluster" {
   source = "../../api-aws-tf-modules/module.ecr-infra"
 
-  default_region = ""
-  enabled = false
-  environment = ""
-  isMonitoring = false
-  max_image_count = 0
-  owner = ""
-  repo_name = ""
-  team = ""
+  default_region = var.default_region
+  team = var.team
+  environment = var.environment
+  isMonitoring = var.isMonitoring
+  owner = var.owner
+
+  enabled = var.enabled
+
+  max_image_count = var.max_image_count
+  repo_name = var.repo_name
 }
