51ee328b58686ad3de4cd7c261166cb28d2b2d38
vivek22117 <vivekmishra22117@gmail.com>
2022-01-26 17:20:00 +0530

    adding output tf script

---
+++ deployment/db-resources/output.tf
@@ -0,0 +1,16 @@
+output "db_cluster_arn" {
+  value = module.auth_api_db_impl.db_cluster_arn
+}
+
+output "db_secret_arn" {
+  value = module.auth_api_db_impl.db_secret_arn
+}
+
+output "db_endpoint" {
+  value = module.auth_api_db_impl.db_endpoint
+}
+
+output "db_reader_endpoint" {
+  value = module.auth_api_db_impl.db_reader_endpoint
+}
+
