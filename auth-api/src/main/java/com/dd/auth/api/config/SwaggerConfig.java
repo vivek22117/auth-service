9b747a6c8866a9a7ef49b5c3f5a9433b994cdcc1
vivek22117 <vivekmishra22117@gmail.com>
2022-05-23 17:45:01 +0530

    adding annotation at model class for swagger documentation

--- auth-api/src/main/java/com/dd/auth/api/config/SwaggerConfig.java
+++ auth-api/src/main/java/com/dd/auth/api/config/SwaggerConfig.java
@@ -11,14 +11,16 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 @Import(SpringDataRestConfiguration.class)
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.dd.auth.api"))
