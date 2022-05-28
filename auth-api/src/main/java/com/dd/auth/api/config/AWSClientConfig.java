33883c22373baf1466bed0e195822ed2f69e125f
vivek22117 <vivekmishra22117@gmail.com>
2022-05-28 21:00:17 +0530

    adding jwt dependency

--- auth-api/src/main/java/com/dd/auth/api/config/AWSClientConfig.java
+++ auth-api/src/main/java/com/dd/auth/api/config/AWSClientConfig.java
@@ -8,12 +8,25 @@ import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.dd.auth.api.util.JwtConfiguration;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.util.DefaultResourceRetriever;
+import com.nimbusds.jose.util.ResourceRetriever;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -22,6 +35,7 @@ public class AWSClientConfig {
 
     private static final Logger logger = getLogger(AWSClientConfig.class);
     private static AWSCredentialsProvider awsCredentialsProvider;
+    private final JwtConfiguration jwtConfiguration;
 
     @Value("${isRunningInEC2: No value}")
     private boolean isRunningInEC2;
@@ -31,6 +45,13 @@ public class AWSClientConfig {
 
     @Value("${awsRegion: No value}")
     private String region;
+
+    @Value(value = "${aws.cognito.userPoolId}")
+    private String userPoolId;
+
+    public AWSClientConfig(JwtConfiguration jwtConfiguration) {
+        this.jwtConfiguration = jwtConfiguration;
+    }
 
     @Bean
     public AmazonSNS createSNSClient() {
@@ -64,7 +85,19 @@ public class AWSClientConfig {
 
     @Bean
     public ObjectMapper getObjectMapper() {
-       return ((new ObjectMapper()).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
+        return ((new ObjectMapper()).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
+    }
+
+    @Bean
+    public ConfigurableJWTProcessor configurableJWTProcessor() throws MalformedURLException {
+        ResourceRetriever resourceRetriever = new DefaultResourceRetriever(jwtConfiguration.getConnectionTimeout(),
+                jwtConfiguration.getReadTimeout());
+        URL jwkSetURL = new URL(jwtConfiguration.getJwkUrl());
+        JWKSource keySource = new RemoteJWKSet<>(jwkSetURL, resourceRetriever);
+        ConfigurableJWTProcessor jwtProcessor = new DefaultJWTProcessor<>();
+        JWSKeySelector keySelector = new JWSVerificationKeySelector(JWSAlgorithm.RS256, keySource);
+        jwtProcessor.setJWSKeySelector(keySelector);
+        return jwtProcessor;
     }
 
     private AWSCredentialsProvider getAWSCredentialProvider() {
