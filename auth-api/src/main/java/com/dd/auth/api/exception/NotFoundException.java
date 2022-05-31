6ac0f63c6a85da149b55556aaf6c4cb9d7935e0d
vivek22117 <vivekmishra22117@gmail.com>
2022-05-31 08:10:50 +0530

    updating NotFoundException Class

---
+++ auth-api/src/main/java/com/dd/auth/api/exception/NotFoundException.java
@@ -0,0 +1,22 @@
+package com.dd.auth.api.exception;
+
+import com.sun.corba.se.spi.ior.ObjectId;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(Class<?> clazz, long id) {
+        super(String.format("Entity %s with id %d not found", clazz.getSimpleName(), id));
+    }
+
+    public NotFoundException(Class<?> clazz, String id) {
+        super(String.format("Entity %s with id %s not found", clazz.getSimpleName(), id));
+    }
+
+    public NotFoundException(Class<?> clazz, ObjectId id) {
+        super(String.format("Entity %s with id %s not found", clazz.getSimpleName(), id.toString()));
+    }
+}
