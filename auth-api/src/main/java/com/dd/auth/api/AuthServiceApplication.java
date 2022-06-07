c5b9551b965f5a87b8b8543e9e3618d5b54ccad6
vivek22117 <vivekmishra22117@gmail.com>
2022-06-07 09:26:47 +0530

    removing unwanted imports

--- auth-api/src/main/java/com/dd/auth/api/AuthServiceApplication.java
+++ auth-api/src/main/java/com/dd/auth/api/AuthServiceApplication.java
@@ -4,7 +4,6 @@ import com.dd.auth.api.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 @Import(SwaggerConfig.class)
