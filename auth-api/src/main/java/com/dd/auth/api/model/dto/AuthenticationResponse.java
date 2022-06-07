3de9e989099c1016d9c44b455216aad0af3d6c2c
vivek22117 <vivekmishra22117@gmail.com>
2022-06-07 09:21:34 +0530

    removing unused annotation

--- auth-api/src/main/java/com/dd/auth/api/model/dto/AuthenticationResponse.java
+++ auth-api/src/main/java/com/dd/auth/api/model/dto/AuthenticationResponse.java
@@ -1,6 +1,5 @@
 package com.dd.auth.api.model.dto;
 
-import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +11,6 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@ApiModel
 public class AuthenticationResponse {
     private String authenticationToken;
     private String idToken;
