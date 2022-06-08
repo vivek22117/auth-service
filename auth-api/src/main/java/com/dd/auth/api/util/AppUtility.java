304778ce9748c88529a2636f783e1466904cf9a3
vivek22117 <vivekmishra22117@gmail.com>
2022-06-08 10:03:39 +0530

    updating API paths

--- auth-api/src/main/java/com/dd/auth/api/util/AppUtility.java
+++ auth-api/src/main/java/com/dd/auth/api/util/AppUtility.java
@@ -7,10 +7,10 @@ public class AppUtility {
 
     public static final String API_AUTH_ROOT_URI = "/api";
     public static final String AUTH_SIGNUP_URI = "/public/auth/signup";
-    public static final String ACCOUNT_VERIFICATION_TOKEN_URI = "/auth/accountVerification/{token}";
+    public static final String ACCOUNT_VERIFICATION_TOKEN_URI = "/public/auth/accountVerification/{token}";
     public static final String AUTH_LOGIN_URI = "/public/auth/login";
     public static final String AUTH_REFRESH_TOKEN_URI = "/auth/refresh/token";
-    public static final String AUTH_LOGOUT_URI = "/public/auth/logout";
+    public static final String AUTH_LOGOUT_URI = "/auth/logout";
     public static final String AUTH_CHANGE_PASSWORD_URI = "/auth/changepassword";
     public static final String AUTH_PUBLIC_KEY_URI = "/auth/.well-know/jwks.json";
 
