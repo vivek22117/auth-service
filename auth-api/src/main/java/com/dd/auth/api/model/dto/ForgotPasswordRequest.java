b8c56bef88ef4e944f937f66feddb5888cd3289e
vivek22117 <vivekmishra22117@gmail.com>
2022-06-09 20:54:11 +0530

    updating password request pojo

--- auth-api/src/main/java/com/dd/auth/api/model/dto/ForgotPasswordRequest.java
+++ auth-api/src/main/java/com/dd/auth/api/model/dto/ForgotPasswordRequest.java
@@ -5,11 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ForgotPasswordRequest {
 
+    @NotBlank
     private String email;
 }
