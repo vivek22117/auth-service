6bc193462a89bf32b8843922c0d83e18c16ac1f0
vivek22117 <vivekmishra22117@gmail.com>
2022-06-09 20:52:21 +0530

    adding new class for api response

---
+++ auth-api/src/main/java/com/dd/auth/api/model/dto/Message.java
@@ -0,0 +1,18 @@
+package com.dd.auth.api.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Message {
+
+    private String message;
+
+    private String status;
+
+}
