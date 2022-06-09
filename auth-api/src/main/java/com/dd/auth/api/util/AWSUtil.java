8243eb1abf7a1f805a4a720f387e37ba61638dd5
vivek22117 <vivekmishra22117@gmail.com>
2022-06-09 20:54:40 +0530

    updating password request pojo

--- auth-api/src/main/java/com/dd/auth/api/util/AWSUtil.java
+++ auth-api/src/main/java/com/dd/auth/api/util/AWSUtil.java
@@ -1,8 +1,11 @@
 package com.dd.auth.api.util;
 
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
+import com.dd.auth.api.exception.ApplicationException;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,13 +20,15 @@ public class AWSUtil {
     private static final Logger LOGGER = getLogger(AWSUtil.class);
 
     private final AmazonSNS snsClient;
+    private final AmazonSimpleEmailService emailService;
 
     @Value("${aws.sns.arn: No Value}")
     private String snsARN;
 
     @Autowired
-    public AWSUtil(AmazonSNS snsClient) {
+    public AWSUtil(AmazonSNS snsClient, AmazonSimpleEmailService emailService) {
         this.snsClient = snsClient;
+        this.emailService = emailService;
     }
 
     public void publish(Map<String, String> snsMessage) {
@@ -35,5 +40,23 @@ public class AWSUtil {
         PublishResult result = snsClient.publish(request);
 
         LOGGER.debug("SNS message published messageId: {} ", result.getMessageId());
+    }
+
+    public void sendEmail(String emailContent, String senderEmail, String receiverEmail, String emailSubject) {
+        try {
+            SendEmailRequest sendEmailRequest = new SendEmailRequest()
+                    .withDestination(
+                            new Destination().withToAddresses(receiverEmail))
+                    .withMessage(new Message()
+                            .withBody(new Body().withHtml(
+                                    new Content().withCharset("UTF-8").withData(emailContent)))
+                            .withSubject(new Content().withCharset("UTF-8").withData(emailSubject)))
+                    .withSource(senderEmail);
+            SendEmailResult sendEmailResult = emailService.sendEmail(sendEmailRequest);
+
+            LOGGER.debug("SES message published messageId: {} ", sendEmailResult.getMessageId());
+        } catch (Exception ex) {
+            throw new ApplicationException("SES send email api call failed!" + ex.getMessage());
+        }
     }
 }
