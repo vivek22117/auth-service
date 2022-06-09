1aee447b650332a1cc38ad1c46292da506f14f5e
vivek22117 <vivekmishra22117@gmail.com>
2022-06-09 10:57:33 +0530

    adding forgot password request class

---
+++ auth-api/src/main/java/com/dd/auth/api/model/dto/ForgotPasswordRequest.java
@@ -0,0 +1,15 @@
+package com.dd.auth.api.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ForgotPasswordRequest {
+
+    private String email;
+}
