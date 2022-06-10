18904263daee5104ca8eb9073919feef79871d50
vivek22117 <vivekmishra22117@gmail.com>
2022-06-10 10:10:10 +0530

    adding SES service class

--- auth-api/src/main/java/com/dd/auth/api/service/SESEmailService.java
+++ auth-api/src/main/java/com/dd/auth/api/service/SESEmailService.java
@@ -1,15 +1,10 @@
 package com.dd.auth.api.service;
 
-import com.amazonaws.services.simpleemail.model.*;
-import com.dd.auth.api.exception.ApplicationException;
 import com.dd.auth.api.model.NotificationEmail;
 import com.dd.auth.api.util.AWSUtil;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -22,22 +17,6 @@ public class SESEmailService {
     @Autowired
     public SESEmailService(AWSUtil awsUtil) {
         this.awsUtil = awsUtil;
-    }
-
-    void sendMail(NotificationEmail notificationEmail) {
-        Map<String, String> snsMessage = new HashMap<>();
-
-        snsMessage.put("From", "vivek.mishra@doubledigit-solutions.com");
-        snsMessage.put("To", notificationEmail.getRecipient());
-        snsMessage.put("Subject", notificationEmail.getSubject());
-        snsMessage.put("Text", notificationEmail.getBody());
-
-        try {
-            awsUtil.publish(snsMessage);
-            LOGGER.info("Activation mail sent!!");
-        } catch (Exception ex) {
-            throw new ApplicationException("Exception occurred when sending mail to " + notificationEmail.getRecipient());
-        }
     }
 
     public void sendEmail(NotificationEmail notificationEmail) {
@@ -57,7 +36,7 @@ public class SESEmailService {
                 "</html>";
 
         String senderEmail = "vivek.mishra@doubledigit-solutions.com";
-        String receiverEmail =  notificationEmail.getRecipient();
+        String receiverEmail = notificationEmail.getRecipient();
         String emailSubject = notificationEmail.getSubject();
 
         awsUtil.sendEmail(emailContent, senderEmail, receiverEmail, emailSubject);
