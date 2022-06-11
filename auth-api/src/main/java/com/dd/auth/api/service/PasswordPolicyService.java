82f19605b5ac6ac19cdcfd5c116c276b231f7f7b
vivek22117 <vivekmishra22117@gmail.com>
2022-06-11 08:39:45 +0530

    updating password policy service

--- auth-api/src/main/java/com/dd/auth/api/service/PasswordPolicyService.java
+++ auth-api/src/main/java/com/dd/auth/api/service/PasswordPolicyService.java
@@ -1,23 +1,31 @@
 package com.dd.auth.api.service;
 
 import com.amazonaws.services.cognitoidp.model.UserNotFoundException;
+import com.dd.auth.api.entity.Login;
+import com.dd.auth.api.entity.PasswordResetToken;
 import com.dd.auth.api.entity.Profile;
 import com.dd.auth.api.exception.ApplicationException;
+import com.dd.auth.api.exception.NotFoundException;
 import com.dd.auth.api.model.NotificationEmail;
 import com.dd.auth.api.model.dto.ForgotPasswordRequest;
-import com.dd.auth.api.model.dto.Message;
 import com.dd.auth.api.model.dto.ResetPasswordRequest;
+import com.dd.auth.api.repository.LoginRepository;
+import com.dd.auth.api.repository.PasswordResetTokenRepository;
 import com.dd.auth.api.repository.ProfileRepository;
 import com.dd.auth.api.util.AppUtility;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.ValidationException;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import static com.dd.auth.api.util.AppUtility.API_AUTH_ROOT_URI;
+import static com.dd.auth.api.util.AppUtility.ENDPOINT_URL;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @AllArgsConstructor
@@ -28,27 +36,40 @@ public class PasswordPolicyService {
     private static final Logger LOGGER = getLogger(PasswordPolicyService.class);
 
     private final ProfileRepository profileRepository;
+    private final LoginRepository loginRepository;
     private final SESEmailService sesEmailService;
     private final PasswordEncoder passwordEncoder;
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
 
     @Transactional
     public void updateResetPasswordToken(ForgotPasswordRequest forgotPasswordRequest)
             throws UserNotFoundException {
         String email = forgotPasswordRequest.getEmail();
-        String token = UUID.randomUUID().toString();
-        try {
-            String resetPasswordLink = AppUtility.ENDPOINT_URL + AppUtility.API_AUTH_ROOT_URI + "/reset-password?token=" + token;
 
+        if (!AppUtility.emailValidator(email)) {
+            throw new ValidationException("Please enter a valid email address");
+        }
+
+        Profile profile = profileRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException("This email is not registered in our website"));
+
+        try {
+            PasswordResetToken token = new PasswordResetToken();
+            token.setProfile(profile);
+            token.setToken(UUID.randomUUID().toString());
+            token.setExpirationDate(LocalDateTime.now().plusMinutes(30));
+            token = passwordResetTokenRepository.save(token);
+
+            String resetPasswordLink = ENDPOINT_URL + API_AUTH_ROOT_URI
+                    + "/auth/public/reset_password?token="
+                    + token.getToken();
             NotificationEmail notificationEmail = NotificationEmail.builder()
                     .subject("Forgot password link")
                     .recipient(email)
-                    .body(resetPasswordLink)
+                    .body(AppUtility.resetPasswordEmailContent(resetPasswordLink, profile.getFirstName()))
                     .build();
+
             sesEmailService.sendEmail(notificationEmail);
-            Profile profile = profileRepository.findByEmail(email)
-                    .orElseThrow(() -> new ApplicationException("Invalid email id"));
-            profile.setResetToken(token);
-            profileRepository.save(profile);
         } catch (UserNotFoundException ex) {
             LOGGER.error("Exception while sending processing ForgotPasswordRequest message" + ex.getMessage());
             throw new ApplicationException("Exception while sending password reset link!");
@@ -56,18 +77,23 @@ public class PasswordPolicyService {
     }
 
     public void getByResetPasswordToken(ResetPasswordRequest request) {
-        String token = request.getToken();
-        String password = request.getPassword();
-        Profile profile = profileRepository.findByResetToken(token)
-                .orElseThrow(() -> new ApplicationException("You've encountered some errors while trying to reset your password."));
+        PasswordResetToken token = passwordResetTokenRepository.findByToken(request.getToken())
+                .orElseThrow(() -> new NotFoundException("You've encountered some errors while trying to reset your password."));
 
-        updatePassword(profile, password);
+        updatePassword(token.getProfile(), request.getPassword());
+        passwordResetTokenRepository.delete(token);
     }
 
+    @Modifying
     public void updatePassword(Profile profile, String newPassword) {
+        LOGGER.debug("Updating new password" + newPassword);
         String encodedPassword = passwordEncoder.encode(newPassword);
         profile.setPassword(encodedPassword);
-        profile.setResetToken(null);
         profileRepository.save(profile);
+
+        Login login = profile.getLogin();
+        login.setPassword(newPassword);
+        login.setPassphrase(passwordEncoder.encode(newPassword));
+        loginRepository.save(login);
     }
 }
