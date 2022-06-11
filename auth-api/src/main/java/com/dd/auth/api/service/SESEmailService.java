d448c544d63581fa4816e1c70f23d332e48b57e8
vivek22117 <vivekmishra22117@gmail.com>
2022-06-11 08:43:03 +0530

    updating password request pojo

--- auth-api/src/main/java/com/dd/auth/api/service/SESEmailService.java
+++ auth-api/src/main/java/com/dd/auth/api/service/SESEmailService.java
@@ -21,25 +21,11 @@ public class SESEmailService {
 
     public void sendEmail(NotificationEmail notificationEmail) {
 
-        String emailContent = "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"utf-8\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
-                "    <title>Example HTML Email</title>\n" +
-                "</head>\n" +
-                "<body style=\"background: whitesmoke; padding: 30px; height: 100%\">\n" +
-                "<h5 style=\"font-size: 18px; margin-bottom: 6px\">Dear example,</h5>\n" +
-                "<p style=\"font-size: 16px; font-weight: 500\">Greetings from TutorialsBuddy</p>\n" +
-                "<p>This is a simple html based email.</p>\n" +
-                "</body>\n" +
-                "</html>";
-
-        String senderEmail = "vivek.mishra@doubledigit-solutions.com";
+        String senderEmail = "admin@doubledigit-solutions.com";
         String receiverEmail = notificationEmail.getRecipient();
         String emailSubject = notificationEmail.getSubject();
 
-        awsUtil.sendEmail(emailContent, senderEmail, receiverEmail, emailSubject);
+        awsUtil.sendEmail(notificationEmail.getBody(), senderEmail, receiverEmail, emailSubject);
     }
 
 }
