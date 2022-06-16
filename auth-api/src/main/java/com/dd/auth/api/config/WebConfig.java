08eead1f75be4641a7cf298938cd6f41ca80c439
vivek22117 <vivekmishra22117@gmail.com>
2022-06-16 19:45:50 +0530

    Updating cors config

--- auth-api/src/main/java/com/dd/auth/api/config/WebConfig.java
+++ auth-api/src/main/java/com/dd/auth/api/config/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
